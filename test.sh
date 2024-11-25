3f42dfd6d6528e409b16268a28732b99228033cf
eshedortal <167910190+eshedortal@users.noreply.github.com>
2024-11-25 19:11:23 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-cat 1.txt
-cat 2.txt
+if [[ -f "1.txt" ]]; then
+  echo "Contents of 1.txt:"
+  cat 1.txt
+else
+  echo "1.txt not found!"
+fi
+
+if [[ -f "2.txt" ]]; then
+  echo "Contents of 2.txt:"
+  cat 2.txt
+else
+  echo "2.txt not found!"
+fi
